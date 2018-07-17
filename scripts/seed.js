b5d284afeac0ecb55bf05b49fe30eefb27eaeadf
nodkz <pavel.chertorogov@gmail.com>
2018-07-17 17:44:05 +0600

    fix: seed file properly obtain db name

--- scripts/seed.js
+++ scripts/seed.js
@@ -6,6 +6,12 @@ import { MongoClient } from 'mongodb';
 import fs from 'fs';
 import { getExampleNames, resolveExamplePath, mongoUri } from '../config';
 
+function getDBName(uri: string) {
+  const m = uri.match(/\d{5}\/([a-z0-9_-]{1,})/i);
+  if (m && m[1]) return m[1];
+  return 'graphql-compose-mongoose';
+}
+
 let db;
 async function run() {
   const con = await MongoClient.connect(
@@ -13,7 +19,7 @@ async function run() {
     { useNewUrlParser: true }
   );
 
-  db = con.db('graphql-compose-mongoose');
+  db = con.db(getDBName(mongoUri));
 
   const exampleNames = getExampleNames();
   for (const name of exampleNames) {
