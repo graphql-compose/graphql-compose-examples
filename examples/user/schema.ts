e6f8385b839b84a0406e4417e4f4db61c57b391f
nodkz <pavel.chertorogov@gmail.com>
2021-06-24 03:27:19 +0600

    refactor: `examples/user` on graphql-compose-mongoose syntax

--- examples/user/schema.ts
+++ examples/user/schema.ts
@@ -11,27 +11,55 @@ import { UserTC } from './models/user';
 
 // create GraphQL Schema with all available resolvers for User Type
 schemaComposer.Query.addFields({
-  userById: UserTC.getResolver('findById'),
-  userByIds: UserTC.getResolver('findByIds'),
-  userOne: UserTC.getResolver('findOne'),
-  userMany: UserTC.getResolver('findMany'), // .debug(), // debug info to console for this resolver
-  userTotal: UserTC.getResolver('count'),
-  userConnection: UserTC.getResolver('connection'),
-  userPagination: UserTC.getResolver('pagination'),
+  userById: UserTC.mongooseResolvers.findById(),
+  userByIds: UserTC.mongooseResolvers.findByIds(),
+  userOne: UserTC.mongooseResolvers.findOne(),
+  userMany: UserTC.mongooseResolvers.findMany().addFilterArg({
+    name: 'geoDistance',
+    type: `input GeoDistance {
+      lng: Float!
+      lat: Float!
+      # Distance in meters
+      distance: Float!
+    }`,
+    description: 'Search by distance in meters',
+    query: (rawQuery, value) => {
+      if (!value.lng || !value.lat || !value.distance) return;
+      // read more https://docs.mongodb.com/manual/tutorial/query-a-2dsphere-index/
+      rawQuery['address.geo'] = {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [value.lng, value.lat],
+          },
+          $maxDistance: value.distance, // <distance in meters>
+        },
+      };
+    },
+  }),
+  // /* FOR DEBUG */
+  //   .debug()
+  // /* OR MORE PRECISELY */
+  //   .debugParams()
+  //   .debugPayload()
+  //   .debugExecTime()
+  userTotal: UserTC.mongooseResolvers.count(),
+  userConnection: UserTC.mongooseResolvers.connection(),
+  userPagination: UserTC.mongooseResolvers.pagination(),
 });
 
 // For debug purposes you may display resolver internals in the following manner:
 // console.log(UserTC.getResolver('findMany').toString());
 
 schemaComposer.Mutation.addFields({
-  userCreate: UserTC.getResolver('createOne'),
-  userCreateMany: UserTC.getResolver('createMany'),
-  userUpdateById: UserTC.getResolver('updateById'),
-  userUpdateOne: UserTC.getResolver('updateOne'),
-  userUpdateMany: UserTC.getResolver('updateMany'),
-  userRemoveById: UserTC.getResolver('removeById'),
-  userRemoveOne: UserTC.getResolver('removeOne'),
-  userRemoveMany: UserTC.getResolver('removeMany'),
+  userCreate: UserTC.mongooseResolvers.createOne(),
+  userCreateMany: UserTC.mongooseResolvers.createMany(),
+  userUpdateById: UserTC.mongooseResolvers.updateById(),
+  userUpdateOne: UserTC.mongooseResolvers.updateOne(),
+  userUpdateMany: UserTC.mongooseResolvers.updateMany(),
+  userRemoveById: UserTC.mongooseResolvers.removeById(),
+  userRemoveOne: UserTC.mongooseResolvers.removeOne(),
+  userRemoveMany: UserTC.mongooseResolvers.removeMany(),
 });
 
 const graphqlSchema = schemaComposer.buildSchema();
